f5ebb5177a1a9b69c92043e091168637054bb4c0
Valentin Brandl <vbrandl@riseup.net>
2020-02-15 21:11:46 +0100

    Build using nix

---
+++ default.nix
@@ -0,0 +1,13 @@
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs { }
+, naersk ? pkgs.callPackage sources.naersk { }
+}:
+
+naersk.buildPackage {
+  src = builtins.filterSource
+    (path: type: type != "directory" || builtins.baseNameOf path != "target")
+    ./.;
+
+  buildInputs = [ pkgs.openssl ];
+  doCheck = false;
+}
